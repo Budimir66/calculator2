5c1ee2f7a80a886b7cc72f0a9ef6a479f07c8a92
Budimir66 <budimir@bk.ru>
2024-06-19 13:33:25 +0300

    4 commit

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -1,59 +1,40 @@
-
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        Scanner sc = new Scanner(System.in);   //  ввывод на консоль
+        System.out.println("Input:");
+        String s = reader.readLine();
+        String[] arrS = s.split(" ");
 
-        System.out.println("Input: ");
-        int[] number = new int[11];
-        for (int i = 0; i < 11; i++)
-        {
-            number[i] = sc.nextInt();
-            int a = number[i];
 
-            char w = sc.next().charAt(0);
-            number[i] = sc.nextInt();
-            int b = number[i];
-            if (a > 10) {System.out.println("Значение больше 10 не удовлетворяет условиям");}
-            else if (a < 1) {System.out.println("Значение меньше 0 не удовлетворяет условиям");}
-            else if (b > 10) {System.out.println("Значение больше 10 не удовлетворяет условиям");}
-            else if (b < 1) {System.out.println("Значение меньше 0 не удовлетворяет условиям");}
+        int num1 = Integer.parseInt(arrS[0]);
+        int num2 = Integer.parseInt(arrS[2]);
 
-            else if (w == '+') {
-                int r = a + b;
-                System.out.println("Output: "  + r );
+        int res = 0;
 
-            }
-            else if (w == '-') {
-                int r = a - b;
-                System.out.println("Output: "  + r );
-
-            }
-
-            else if (w == '*') {
-                int r = a * b;
-                System.out.println("Output: "  + r );
-
-            }
-            else  if (w == '/') {
-                int r = a / b;
-                System.out.println("Output: "  + r );
-
-            }
-            else {System.out.println("строка не является математической операцией");
-
-            }
-
+        switch (arrS[1]){
+            case "+":
+                res = num1 + num2;
+                break;
+            case "-":
+                res = num1 - num2;
+                break;
+            case "*":
+                res = num1 * num2;
+                break;
+            case "/":
+                res = num1 / num2;
+                break;
         }
 
-
-
-
-
+        System.out.println(res);
     }
-
 }
+
+
+
 
